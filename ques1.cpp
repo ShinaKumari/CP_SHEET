a04ae0bab9ac670330200f95cc498875e51cbd0a
Shina Kumari <theshina004@gmail.com>
2023-07-02 21:07:57 +0530

    first commit

---
+++ ques1.cpp
@@ -0,0 +1,13 @@
+// problem is LCHIHIME AND TRIANGLE they should be form a triangle
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+return 0;
+}
